918d47e0bcdd38074ea0a9ed2bcb1b6e43cf4e56
Ali Zobeid <Alizobeid@gmail.com>
2024-08-11 00:45:54 +0330

    add apps in setting.py file

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    ### my apps ###
+    "accounts",
+    "vendors",
+    "customers",
+    "admin_panel",
+    "cart",
+    "website"
 ]
 
 MIDDLEWARE = [
