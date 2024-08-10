002567894307a54314f05a712c78088399bbbb74
Ali Zobeid <Alizobeid@gmail.com>
2024-08-10 16:16:43 +0330

    app(vendors)[models] : add Owner model

--- src/vendors/models.py
+++ src/vendors/models.py
@@ -1,8 +1,18 @@
 from django.db import models
-from accounts.models import User
+from accounts.models import User, Address
+
+
+class Owner(User):
+    class Meta:
+        proxy = True
+
+    def save(self, *args, **kwargs):
+        self.is_owner = True
+        return super(Owner, self).save(*args, **kwargs)
+
 
 class Vendor(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     company_name = models.CharField(max_length=255)
     desciption = models.TextField()
 
