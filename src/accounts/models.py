d3ea1cd64b1737efcc3c8f7f340064d79330ef6b
Ali Zobeid <Alizobeid@gmail.com>
2024-08-10 11:22:45 +0330

    app(accounts)[models] : add CreateUserManager model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import UserManager, AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class CreateUserManager(UserManager):
+    def _create_user(self,email,password,**extra_fields):
+        if not email:
+            raise ValueError("email not validated")
+        email = self.normalize_email(email=email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self,email=None,password=None,**extra_fields):
+        extra_fields.setdefault("is_staff",False)
+        extra_fields.setdefault("is_superuser",False)
+        return self._create_user(email,password,**extra_fields)
+    
+    def create_superuser(self,email=None,password=None,**extra_fields):
+        extra_fields.setdefault("is_staff",True)
+        extra_fields.setdefault("is_superuser",True)
+        return self._create_user(email,password,**extra_fields)
