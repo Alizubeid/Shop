64c95ff67bba90471cbd33c5a6664b7fd145c7e5
Ali Zobeid <Alizobeid@gmail.com>
2024-08-11 01:08:52 +0330

    add static dirctory and add defualt sets in config/setting.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -133,3 +133,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
