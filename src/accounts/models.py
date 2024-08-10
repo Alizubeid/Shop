323f998b38e997536178f14577bc435149c1be93
Ali Zobeid <Alizobeid@gmail.com>
2024-08-10 11:30:28 +0330

    app(accounts)[models] : add User model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import UserManager, AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
 
 
 class CreateUserManager(UserManager):
-    def _create_user(self,email,password,**extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError("email not validated")
         email = self.normalize_email(email=email)
@@ -11,13 +12,30 @@ class CreateUserManager(UserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
-    
-    def create_user(self,email=None,password=None,**extra_fields):
-        extra_fields.setdefault("is_staff",False)
-        extra_fields.setdefault("is_superuser",False)
-        return self._create_user(email,password,**extra_fields)
-    
-    def create_superuser(self,email=None,password=None,**extra_fields):
-        extra_fields.setdefault("is_staff",True)
-        extra_fields.setdefault("is_superuser",True)
-        return self._create_user(email,password,**extra_fields)
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    is_customer = models.BooleanField(default=False)
+    is_owner = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now())
+    date_login = models.DateTimeField(blank=True, null=True)
+
+    # customiz fields
+    USERNAME_FIELD = "email"
+    EMIAL_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    # where creation object
+    objects = CreateUserManager()
