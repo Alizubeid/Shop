e14e1bd248d30c7dd352b9414c72cfdf3d9aae8c
Ali Zobeid <Alizobeid@gmail.com>
2024-08-11 00:38:45 +0330

    app(cart)[models] : add Discount model

--- src/cart/models.py
+++ src/cart/models.py
@@ -1,24 +1,35 @@
 from django.db import models
 from vendors.models import Company
+from accounts.models import User
+
 
 class Product(models.Model):
     name = models.CharField(max_length=64)
     price = models.PositiveIntegerField()
     date_pd = models.DateField()
     date_ex = models.DateField()
-    company = models.ForeignKey(Company,on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
     category = models.ManyToManyField("Category")
+
 
 class Category(models.Model):
     category = models.CharField(max_length=64)
-    sub_category = models.ForeignKey("self",null=True,on_delete=models.CASCADE)
+    sub_category = models.ForeignKey("self", null=True, on_delete=models.CASCADE)
+
 
 class Property(models.model):
     property_field = models.CharField(max_length=64)
 
+
 class ProductProperties(models.Model):
-    product = models.ForeignKey(Product,on_delete=models.CASCADE)
-    property_field = models.ForeignKey(Property,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    property_field = models.ForeignKey(Property, on_delete=models.CASCADE)
     value = models.CharField(max_length=64)
 
 
+class Discount(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    amount = models.PositiveIntegerField(null=True)
+    percent = models.PositiveIntegerField(null=True)
