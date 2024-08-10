03b036f8cebe72655fe20ea5bfb38c0de31599d9
Ali Zobeid <Alizobeid@gmail.com>
2024-08-10 19:15:17 +0330

    app(cart)[models] : add Product model

--- src/cart/models.py
+++ src/cart/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from vendors.models import Company
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.PositiveIntegerField()
+    date_pd = models.DateField()
+    date_ex = models.DateField()
+    company = models.ForeignKey(Company,on_delete=models.CASCADE)
+
+
