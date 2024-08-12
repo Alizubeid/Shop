f217ff4b5e0fed19648bf4814cbb83128f998068
Ali Zobeid <Alizobeid@gmail.com>
2024-08-12 11:10:40 +0330

    app(accounts)[views] : create SignUpView CBV

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+from accounts.forms import SignupForm
 
-# Create your views here.
+class SignUpView(FormView):
+    form_class = SignupForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
+    
