ff44d6eaa7d58584afc0150cf85a8706d7788725
Ali Zobeid <Alizobeid@gmail.com>
2024-08-12 10:53:12 +0330

    app(cart)[models] : Update category model

--- src/cart/models.py
+++ src/cart/models.py
@@ -3,21 +3,26 @@ from vendors.models import Company
 from accounts.models import User
 
 
+class Category(models.Model):
+    category = models.CharField(max_length=64)
+    sub_category = models.ForeignKey(
+        "self",
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="category_sub_category",
+    )
+
+
 class Product(models.Model):
     name = models.CharField(max_length=64)
     price = models.PositiveIntegerField()
     date_pd = models.DateField()
     date_ex = models.DateField()
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    category = models.ManyToManyField("Category")
+    category = models.ManyToManyField(Category)
 
 
-class Category(models.Model):
-    category = models.CharField(max_length=64)
-    sub_category = models.ForeignKey("self", null=True, on_delete=models.CASCADE)
-
-
-class Property(models.model):
+class Property(models.Model):
     property_field = models.CharField(max_length=64)
 
 
@@ -28,18 +33,20 @@ class ProductProperties(models.Model):
 
 
 class Discount(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE,null=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE,null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     amount = models.PositiveIntegerField(null=True)
     percent = models.PositiveIntegerField(null=True)
 
+
 class Cart(models.Model):
-    customer = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
     total_amount = models.PositiveIntegerField(default=0)
     date = models.DateField()
     is_paid = models.BooleanField(default=False)
 
+
 class CartItems(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE,null=True)
-    item = models.ForeignKey(Product, on_delete=models.CASCADE,null=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    item = models.ForeignKey(Product, on_delete=models.CASCADE)
