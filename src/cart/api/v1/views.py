6639fb9f8fcf1e3034f84af986f541ad2ac6b3e1
Ali Zobeid <Alizobeid@gmail.com>
2024-08-25 13:59:46 +0330

    fix: api view

--- src/cart/api/v1/views.py
+++ src/cart/api/v1/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from .serializer import ProductSerializer
 from cart.models import Product
 
-class ProductAPIView(generics.ListAPIView):
-    serializer_class = ProductSerializer
-    queryset = Product.objects.all()
+class ProductAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        obj = Product.objects.all().first()
+        serialaizer = ProductSerializer(obj)
+        # serialaizer.is_valid(raise_exception=True)
+        return Response(serialaizer.data)
